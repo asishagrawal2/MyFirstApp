b4a80376e973006cde006c95e64cc598711284dd
asish <asish.agrwl@gmail.com>
2012-04-12 13:47:55 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Assapp::Application.config.secret_token = '48e620063f5d7625502ca7df0e8972fef5742c934f707c688c8d17b51529cf6d7c38d9c3b082ae8820086255e9b778c1d282d750560dc49f9fe977fd78314245'
